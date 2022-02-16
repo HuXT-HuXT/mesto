9c75e7b9fa575715fe6c9021e255d24760a8793a
Konstantin Suvorov <huxt-huxt@yandex.ru>
2022-02-16 13:28:03 +0300

    4th_project_2nd_fix

--- script/index.js
+++ script/index.js
@@ -2,10 +2,11 @@ const wholePage = document.querySelector('.page');
 const profile = wholePage.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
 const popup = wholePage.querySelector('.popup');
+const popupForm = popup.querySelector('.popup__form');
 const closeButton = popup.querySelector('.popup__close-button');
 
-let nameInput = popup.querySelector('.popup__input_name');
-let aboutInput = popup.querySelector('.popup__input_about');
+let nameInput = popup.querySelector('.popup__input_field_name');
+let aboutInput = popup.querySelector('.popup__input_field_about');
 
 let oldName = profile.querySelector('.profile__name');
 let oldAbout = profile.querySelector('.profile__short-about');
@@ -36,5 +37,5 @@ editButton.addEventListener('click', showPopup);
 
 closeButton.addEventListener('click', hidePopup);
 
-popup.addEventListener('submit', formSubmitHandler);
+popupForm.addEventListener('submit', formSubmitHandler);
 
