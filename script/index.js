6205db8fbc3cba76107d2403b6980ac6be4fc29b
Konstantin Suvorov <huxt-huxt@yandex.ru>
2022-03-10 21:41:11 +0300

    5th_project_3rd_fix

--- script/index.js
+++ script/index.js
@@ -16,8 +16,8 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Беэр-шева',
+    link: 'https://i0.wp.com/lh3.googleusercontent.com/-3L7E-Hx6HbE/VVxtVN6WwuI/AAAAAAABQpU/41qr2NOA2Q8/s1200/i_b7_078_20150408_5D3_0284.jpg?w=1600&ssl=1'
   },
   {
     name: 'Байкал',
@@ -41,51 +41,47 @@ const editionPopupTitle = editionPopup.querySelector('.popup__title');
 const editionPopupButton = editionPopup.querySelector('.popup__submit');
 const editionPopupNameInput = editionPopup.querySelector('.popup__input_field_name');
 const editionPopupAboutInput = editionPopup.querySelector('.popup__input_field_about');
+const editionPopupClose = editionPopup.querySelector('.popup__close-button');
 const additionPopupForm = additionPopup.querySelector('.popup__form');
 const additionPopupTitle = additionPopup.querySelector('.popup__title');
 const additionPopupButton = additionPopup.querySelector('.popup__submit');
 const additionPopupNameInput = additionPopup.querySelector('.popup__input_field_name');
 const additionPopupLinkInput = additionPopup.querySelector('.popup__input_field_about');
+const additionPopupClose = additionPopup.querySelector('.popup__close-button');
 const photoPopupPhoto = photoPopup.querySelector('.popup__photo');
 const photoPopupTitle = photoPopup.querySelector('.popup__phototitle');
+const photoPopupClose = photoPopup.querySelector('.popup__close-button');
 
 const elementsSection = wholePage.querySelector('.elements');
 
 const template = document.querySelector('#element').content;
 
-function findPopup(evt) {
-  let modalWindow = {};
-  if (evt.target.classList.value === 'profile__edit-button') {
-    modalWindow = editionPopup;
-    editionPopupTitle.innerHTML = 'Редактировать профиль';
-    editionPopupNameInput.placeholder = 'Имя';
-    editionPopupAboutInput.placeholder = 'Описание';
-    editionPopupNameInput.value = profileOldName.textContent;
-    editionPopupAboutInput.value = profileOldAbout.textContent;
-  };
-  if (evt.target.classList.value === 'profile__add-button') {
-    modalWindow = additionPopup;
-    additionPopupTitle.innerHTML = 'Новое место';
-    additionPopupNameInput.placeholder = 'Название';
-    additionPopupLinkInput.placeholder = 'Ссылка на картинку';
-    additionPopupNameInput.value = '';
-    additionPopupLinkInput.value = '';
-  };
-  if (evt.target.className === 'element__photo') {
-    const photoName = evt.target.closest('.element').querySelector('.element__name').textContent;
-    const photoLink = evt.target.closest('.element').querySelector('.element__photo').src;
+/*прошу прощения, сударь, что заставил Вас быть свидетелем проявленными мной костностью и
+зашоренностью мышления.
+надеюсь, сейчас удалось исправить допущенные промахи и оплошности, приблизившись к более-менее
+удобоваримому варианту.
+сомнения меня одолели в части необходимости отдельных функций для обработки кнопок - что предочесть
+отдельная функция или стрелочное представление, выбор пал на отдельные функции.
+не судите строго.*/
 
-    photoPopupTitle.textContent = photoName;
-    photoPopupPhoto.src = photoLink;
-    photoPopupPhoto.alt = photoName;
+function handleEditButton() {
+  editionPopupNameInput.value = profileOldName.textContent;
+  editionPopupAboutInput.value = profileOldAbout.textContent;
+  showPopup(editionPopup);
+}
 
-    modalWindow = photoPopup;
-  };
-  showPopup(modalWindow);
-};
+function handleAddButton() {
+  showPopup(additionPopup);
+}
+
+function handlePhotoEnlargement(evt) {
+  photoPopupTitle.textContent = evt.target.closest('.element').querySelector('.element__name').textContent;
+  photoPopupPhoto.src = evt.target.closest('.element').querySelector('.element__photo').src;
+  photoPopupPhoto.alt = evt.target.closest('.element').querySelector('.element__name').textContent;
+  showPopup(photoPopup);
+}
 
 function showPopup(modalWindow) {
-  modalWindow.querySelector('.popup__close-button').addEventListener('click', closePopup);
   modalWindow.classList.add('popup_opened');
 };
 
@@ -93,55 +89,46 @@ function closePopup(evt) {
   evt.target.closest('.popup').classList.remove('popup_opened');
 };
 
-function handleForm(evt) {
+function handleEditForm(evt) {
   evt.preventDefault();
-  let classToCheck = evt.target.closest('.popup');
-
-  if (classToCheck.classList.contains('popup_type_edit')) {
-
-    let newName = editionPopupNameInput.value;
-    let newAbout = editionPopupAboutInput.value;
-
-    profileOldName.textContent = newName;
-    profileOldAbout.textContent = newAbout;
-  };
-
-  if (classToCheck.classList.contains('popup_type_add')) {
-
-    let newName = additionPopupNameInput.value;
-    let newLink = additionPopupLinkInput.value;
-
-    loadCards(newName, newLink);
-  };
+  profileOldName.textContent = editionPopupNameInput.value;
+  profileOldAbout.textContent = editionPopupAboutInput.value;
 
   closePopup(evt);
-};
+}
 
-function loadCards(newName, newLink) {
+function handleAddForm(evt) {
+  evt.preventDefault();
+  elementsSection.prepend(createCard(additionPopupNameInput.value, additionPopupLinkInput.value));
+
+  closePopup(evt);
+}
+
+function createCard(Name, Link) {
   const elementElement = template.querySelector('.element');
 
   const newElement = elementElement.cloneNode(true);
-  newElement.querySelector('.element__name').textContent = newName;
-  newElement.querySelector('.element__photo').src = newLink;
-  newElement.querySelector('.element__photo').alt = newName;
-  /*add function!!!*/
+  newElement.querySelector('.element__name').textContent = Name;
+  newElement.querySelector('.element__photo').src = Link;
+  newElement.querySelector('.element__photo').alt = Name;
+
   newElement.querySelector('.element__remove-button').addEventListener('click', handleRemoveButton);
-  newElement.querySelector('.element__photo').addEventListener('click', findPopup);
+  newElement.querySelector('.element__photo').addEventListener('click', handlePhotoEnlargement);
   newElement.querySelector('.element__like-button').addEventListener('click', handleHeartButton);
 
-  renderElement(newElement);
+  return newElement;
 };
 
-function renderElement(item) {
-  elementsSection.prepend(item);
+function renderElement(Name, Link) {
+  elementsSection.append(createCard(Name, Link));
 };
 
 function loadInitialCards(array) {
   for(let i = 0; i < array.length; i++) {
-    newName = array[i].name;
-    newLink = array[i].link;
+    Name = array[i].name;
+    Link = array[i].link;
 
-    loadCards(newName, newLink);
+    renderElement(Name, Link);
   }
 };
 
@@ -155,10 +142,16 @@ function handleRemoveButton(evt) {
 
 loadInitialCards(initialCards);
 
-profileButtonEdit.addEventListener('click', findPopup);
+profileButtonEdit.addEventListener('click', handleEditButton);
 
-profileButtonAdd.addEventListener('click', findPopup);
+profileButtonAdd.addEventListener('click', handleAddButton);
 
-editionPopupForm.addEventListener('submit', handleForm);
+editionPopupForm.addEventListener('submit', handleEditForm);
 
-additionPopupForm.addEventListener('submit', handleForm);
+additionPopupForm.addEventListener('submit', handleAddForm);
+
+photoPopupClose.addEventListener('click', closePopup);
+
+additionPopupClose.addEventListener('click', closePopup);
+
+editionPopupClose.addEventListener('click', closePopup);
